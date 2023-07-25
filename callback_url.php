053a02eee0e495102e69a2fceeb4f2387c871f54
Nyamao Gregory Kaiser$ <nyamaokaiser79@gmail.com>
2023-07-25 18:08:23 +0300

    third commit

--- callback_url.php
+++ callback_url.php
@@ -10,7 +10,7 @@
      $mpesaResponse = file_get_contents('php://input');
  
      // log the response
-     $logFile = "M_PESAConfirmationResponse.txt";
+     $logFile = "M_PESAConfirmationResponse.json";
  
      // write to file
      $log = fopen($logFile, "a");
@@ -19,3 +19,18 @@
      fclose($log);
  
      echo $response;
+     //process the mpesa json response data
+$mpesaResponse = file_get_contents('M_PESAConfirmationResponse.json');
+$callbackContent = json_decode($mpesaResponsepesallesponse);
+
+$Resultcode = $callbackContent->Body->stkCallback->ResultCode; 
+$CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
+$Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+$MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->Value; 
+$PhoneNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->Value; 
+$formatedPhone = str_replace("254", "0", $Phonelumber);
+
+if ($Resultcode == 0) {
+
+// Connect to db
+$conn- mysqli_connect("localhost","root","","daraja");
